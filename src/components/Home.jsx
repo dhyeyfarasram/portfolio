cd92f8df79ea33a986f881aa890d92dd818a025b
dhyey-mcmaster <dhyeyfarasram@gmail.com>
2023-02-16 21:58:39 -0800

    original commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
+import HeroImage from "../assets/heroImage.png"
+import {Link} from "react-scroll";
+
+
+const Home = () => {
+  return (
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+            <div className="flex flex-col justify-center h-full">
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">Im a Developer</h2>
+                <p className="text-gray-500 py-4 max-w-md">
+                A motivated individual who demonstrates a strong work ethic and creative ability. Seeking to apply my software development and technical skills at your company. Will leverage proven experience as a competent developer to contribute to company goals and needs. I will work on any project and try to learn the necessary skill set required the 
+job.
+                </p>
+                <div>
+                    <Link to="portfolio" smooth duartion={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                        portfolio
+                        <span className="group-hover:rotate-90 duration-300">
+                            <MdOutlineKeyboardArrowRight size={25} className="ml=1"/>
+                        </span>
+                    </Link>
+                </div>
+            </div>
+            <div><img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full ml-5" /></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
+import installNode from '../assets/portfolio/installNode.jpg'
+import navbar from '../assets/portfolio/navbar.jpg'
+import reactParallax from '../assets/portfolio/reactParallax.jpg'
+import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
+import reactWeather from '../assets/portfolio/reactWeather.jpg'
+
+
+
+const Portfolio = () => {
+
+    const portfolio = [
+        {
+            id: 1,
+            src: arrayDestruct,
+            link: 'https://github.com/Dhyey-Mcmaster/Session-UserAuth'
+        },
+        {
+            id: 2,
+            src: installNode,
+            link: ''
+        },
+        {
+            id: 3,
+            src: navbar,
+            link: ''
+        },
+        {
+            id: 4,
+            src: reactParallax,
+            link: ''
+        },
+        {
+            id: 5,
+            src: reactSmooth,
+            link: ''
+        },
+        {
+            id: 6,
+            src: reactWeather,
+            link: ''
+        },
+    ]
+
+  return (
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+                <p className="py-6">Check out some of my work</p>
+            </div>
+
+            <div  className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+
+            {
+                portfolio.map(({id,src,link})=>(
+                    <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                        <img src={src} alt="NewsLetter Website" className="rounded-md duration-200 hover:scale-105"> 
+                        </img>
+                    </div>
+                ))
+            }
+
+            </div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Portfolio
