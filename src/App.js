66b928ee2abee59a58baf6df18318e17e70ae951
Dhyey <dhyeyfarasram@gmail.com>
2023-03-10 11:19:36 -0800

    Tracking added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
+import React, {useEffect } from 'react';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-260256730-2";
+
+ReactGA.initialize(TRACKING_ID);
+
+
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+
   return (
     <div>
       <NavBar/>
