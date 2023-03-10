84391a9dcbc383266e6bc3b289ef9b3bc5654a9b
Dhyey <dhyeyfarasram@gmail.com>
2023-03-10 11:49:40 -0800

    refactor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactGA.initialize("G-QP8ZWRQNNE");
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
