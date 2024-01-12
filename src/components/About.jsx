c6acc250f3cd313eeadd2f0e0d678d08b3e17f33
Dhyey <55444226+dhyeyfarasram@users.noreply.github.com>
2024-01-12 15:46:13 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             </div>
 
             <p className="text-xl mt-20">
-Exemplary software professional with 3+ years of experience in developing web applications using JavaScript, HTML, and CSS, and integrating third-party APIs to enable seamless user experience. Skilled at developing scalable software solutions, conducting comprehensive testing, and actively participating in code reviews to ensure optimal functionality. Proficient in developing RESTful APIs for improving data exchange efficiency and reducing response time.            </p>
+Exemplary software professional with 3+ years of experience in developing web applications using JavaScript, HTML, and CSS, and integrating third-party APIs to enable seamless user experience. Skilled at developing scalable software solutions, conducting comprehensive testing, and actively participating in code reviews to ensure optimal functionality. Proficient in developing RESTful APIs for improving data exchange efficiency and reducing response time.            
             <br/>
 My passion for full-stack development has enabled me to excel in both frontend and backend technologies. I have hands-on experience in HTML, CSS, JavaScript, React.js, Node.js, Express, cloud and working with databases like MySQL and MongoDB. Leveraging Git and GitHub, I have collaborated effectively on projects and implemented continuous integration for seamless development. 
           <br/>
@@ -18,7 +18,7 @@ Creating intuitive and user-friendly web applications that deliver exceptional u
 Throughout my academic journey and personal projects, I have honed my skills in agile methodologies, test-driven development, and continuous deployment practices. My expertise in algorithms and data structures allows me to optimize application performance and enhance efficiency.
           <br/>
 Dedicated to ongoing learning, I hold certifications in relevant technologies, including Python, React, and Node.js. With a proven track record of meeting deadlines and excelling in team environments, I am confident in my ability to make valuable contributions to any full-stack development team.
-          <br/>
+          <br/></p>
         </div>
     </div>
   )
