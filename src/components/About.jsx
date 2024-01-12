aa11c3e14c8bee4d8114a78af86116ee28f7e79c
Dhyey <55444226+dhyeyfarasram@users.noreply.github.com>
2024-01-12 15:39:28 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,6 +11,18 @@ const About = () => {
             <p className="text-xl mt-20">
 Exemplary software professional with 3+ years of experience in developing web applications using JavaScript, HTML, and CSS, and integrating third-party APIs to enable seamless user experience. Skilled at developing scalable software solutions, conducting comprehensive testing, and actively participating in code reviews to ensure optimal functionality. Proficient in developing RESTful APIs for improving data exchange efficiency and reducing response time.            </p>
             <br/>
+          <p className="text-xl mt-20"> 
+My passion for full-stack development has enabled me to excel in both frontend and backend technologies. I have hands-on experience in HTML, CSS, JavaScript, React.js, Node.js, Express, cloud and working with databases like MySQL and MongoDB. Leveraging Git and GitHub, I have collaborated effectively on projects and implemented continuous integration for seamless development. 
+          </p><br/>
+          <p className="text-xl mt-20"> 
+Creating intuitive and user-friendly web applications that deliver exceptional user experiences is my forte. I take pride in designing responsive front-end interfaces and implementing robust backend functionalities to ensure reliable application performance.          
+          </p><br/>
+          <p className="text-xl mt-20"> 
+Throughout my academic journey and personal projects, I have honed my skills in agile methodologies, test-driven development, and continuous deployment practices. My expertise in algorithms and data structures allows me to optimize application performance and enhance efficiency.
+          </p><br/>
+          <p className="text-xl mt-20"> 
+Dedicated to ongoing learning, I hold certifications in relevant technologies, including Python, React, and Node.js. With a proven track record of meeting deadlines and excelling in team environments, I am confident in my ability to make valuable contributions to any full-stack development team.
+          </p><br/>
         </div>
     </div>
   )
